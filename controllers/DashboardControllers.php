5664acac59dc80b72b3c3fc265c3c18e23f04b5f
Luciano Emmanuel Gatti Flekenstein <luciano.gatti.utn@gmail.com>
2024-09-01 13:17:16 -0300

    Update DashboardControllers.php

--- controllers/DashboardControllers.php
+++ controllers/DashboardControllers.php
@@ -52,13 +52,13 @@ class DashboardControllers{
         //Identifica el proyecto al que pertenece esa URL
         $proyecto = Proyecto::where('url', $token);
 
-        debuguear($proyecto);
         //Verifica si el usuario tiene acceso a ese proyecto
         if($proyecto->propietarioID !== $_SESSION['id']){
             header('Location: /dashboard');
         } 
 
         $titulo = $proyecto->nombre ?? '';//Guarda el nombre del proyecto
+        debuguear($titulo);
 
         $router->render('dashboard/proyecto', [
             'titulo'=>$titulo
